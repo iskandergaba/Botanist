155615f2a7cd6b87e22506badd9d4049aecb01eb
Iskander Gaba <iskandergaba@gmail.com>
2017-02-09 22:46:27 -0500

    Created Settings Screen

--- app/src/main/java/com/scientists/happy/botanist/MainActivity.java
+++ app/src/main/java/com/scientists/happy/botanist/MainActivity.java
@@ -1,5 +1,6 @@
 package com.scientists.happy.botanist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -44,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
