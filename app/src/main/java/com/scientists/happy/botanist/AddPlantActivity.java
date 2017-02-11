c101762c4fe5d268252afbfafdcbfd8fd3c7db70
Christopher Besser <cbesser@seas.upenn.edu>
2017-02-11 01:54:40 -0500

    Simple AddPlant page completed. Species and username stored as extras in intent, cameraroll takes picture and stores image path in intent extras.

---
+++ app/src/main/java/com/scientists/happy/botanist/AddPlantActivity.java
@@ -0,0 +1,122 @@
+// Add a plant to the database
+// @author: Chia George Washington
+package com.scientists.happy.botanist;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class AddPlantActivity extends AppCompatActivity
+{
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    protected ImageButton iButton;
+    protected EditText speciesBox;
+    protected EditText nicknameBox;
+    protected String photoPath;
+    /**
+     * Launch the add plant screen
+     * @param savedInstanceState - Current app state
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_plant);
+        iButton = (ImageButton) findViewById(R.id.imageButton);
+        speciesBox = (EditText) findViewById(R.id.Species);
+        nicknameBox = (EditText) findViewById(R.id.Nickname);
+    }
+
+    /**
+     * Take a picture
+     * @param view - current activity view
+     */
+    protected void onPressImageButton(View view)
+    {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null)
+        {
+            File photoFile = null;
+            try
+            {
+                photoFile = createImageFile();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            if (photoFile != null)
+            {
+                Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+                System.out.println(photoURI.getPath());
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    /**
+     * Update ImageButton
+     * @param requestCode - code for the update request?
+     * @param resultCode - camera result
+     * @param data - camera data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if ((requestCode == REQUEST_IMAGE_CAPTURE) && (resultCode == RESULT_OK))
+        {
+            int targetW = iButton.getWidth();
+            int targetH = iButton.getHeight();
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(photoPath, bmOptions);
+            int photoW = bmOptions.outWidth;
+            int photoH = bmOptions.outHeight;
+            int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+            bmOptions.inJustDecodeBounds = false;
+            bmOptions.inSampleSize = scaleFactor;
+            Bitmap bitmap = BitmapFactory.decodeFile(photoPath, bmOptions);
+            iButton.setImageBitmap(bitmap);
+        }
+    }
+
+    /**
+     * Store the captured image
+     * @return Returns the image
+     * @throws IOException if the write fails
+     */
+    private File createImageFile() throws IOException
+    {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        photoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    /**
+     * Send information back to main page
+     * @param view - current app view
+     */
+    protected void onPressSubmit(View view)
+    {
+        Intent i = new Intent(this, MainActivity.class);
+        i.putExtra("species", speciesBox.getText().toString());
+        i.putExtra("nickname", nicknameBox.getText().toString());
+        i.putExtra("photoPath", photoPath);
+        startActivity(i);
+    }
+}
--- app/src/main/java/com/scientists/happy/botanist/MainActivity.java
+++ app/src/main/java/com/scientists/happy/botanist/MainActivity.java
@@ -1,52 +1,71 @@
 package com.scientists.happy.botanist;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+    /**
+     * Launch app
+     * @param savedInstanceState - app state
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            /**
+             * Handle action button click
+             * @param view - current view
+             */
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View view)
+            {
+                Intent i = new Intent(MainActivity.this, AddPlantActivity.class);
+                startActivity(i);
             }
         });
     }
 
+    /**
+     * Handle options menu
+     * @param menu - options menu
+     * @return Returns success code
+     */
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
+    /**
+     * Handle selected option
+     * @param item - selected option
+     * @return Returns success code
+     */
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
