2b51ec6064360e545232b30205da2d7a6169699f
Antonio Muscarella <amusca@sas.upenn.edu>
2017-02-12 20:29:24 -0500

    Finished ProfileActivity

--- app/src/main/java/com/scientists/happy/botanist/MainActivity.java
+++ app/src/main/java/com/scientists/happy/botanist/MainActivity.java
@@ -1,7 +1,8 @@
 package com.scientists.happy.botanist;
 
-//comment 
+//comment
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,15 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
