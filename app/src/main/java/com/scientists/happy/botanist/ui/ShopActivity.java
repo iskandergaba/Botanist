2090d71981f464f8ec85154bf86a7b13f89d749a
Wendy Zhang <xwyzhx@gmail.com>
2017-04-09 18:47:31 -0400

    Prettier button

--- app/src/main/java/com/scientists/happy/botanist/ui/ShopActivity.java
+++ app/src/main/java/com/scientists/happy/botanist/ui/ShopActivity.java
@@ -5,6 +5,8 @@ package com.scientists.happy.botanist.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 import com.scientists.happy.botanist.R;
 
 
@@ -14,6 +16,23 @@ public class ShopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+        View poopButton = findViewById(R.id.shop_button);
+        poopButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * User clicked update height
+             *
+             * @param v - current view
+             */
+            @Override
+            public void onClick(View v) {
+                doStuff();
+            }
+        });
+    }
+
+    private void doStuff() {
+        // do stuff tho
     }
 
     @Override
