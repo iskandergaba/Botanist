178ccc245a11cd2b6599b82ad3a3e49d7dfdb858
Iskander Gaba <iskandergaba@gmail.com>
2017-04-20 16:34:09 -0400

    Minor visual improvements

--- app/src/main/java/com/scientists/happy/botanist/ui/MainActivity.java
+++ app/src/main/java/com/scientists/happy/botanist/ui/MainActivity.java
@@ -1,20 +1,13 @@
 // Main page
 // @author: Christopher Besser, Iskander Gaba, Antonio Muscarella, and Wendy Zhang
 package com.scientists.happy.botanist.ui;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
-import android.text.format.DateUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
 
@@ -91,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         else if (id == R.id.action_help) {
-            mDatabase.showTutorial(this, loadTutorialItems(), false);
+            mDatabase.showTutorial(this, loadTutorialItems(), true);
         }
         return super.onOptionsItemSelected(item);
     }
