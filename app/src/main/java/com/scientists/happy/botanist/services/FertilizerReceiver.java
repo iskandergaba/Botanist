1eb37b3c57c63126f731977db491c47ddfac2ff6
Iskander Gaba <iskandergaba@gmail.com>
2017-04-25 00:54:56 -0400

    Conflicts resolution (like a politician xD) and minor fixes

--- app/src/main/java/com/scientists/happy/botanist/services/FertilizerReceiver.java
+++ app/src/main/java/com/scientists/happy/botanist/services/FertilizerReceiver.java
@@ -1,6 +1,7 @@
 // Handle notifications to fertilize plants
 // @author: Christopher Besser
 package com.scientists.happy.botanist.services;
+
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -10,9 +11,11 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
+
 import com.scientists.happy.botanist.R;
 import com.scientists.happy.botanist.data.DatabaseManager;
 import com.scientists.happy.botanist.ui.LoginActivity;
+
 import static android.content.Context.NOTIFICATION_SERVICE;
 public class FertilizerReceiver extends BroadcastReceiver {
     /**
@@ -25,7 +28,7 @@ public class FertilizerReceiver extends BroadcastReceiver {
         String name = intent.getExtras().getString("name");
         String plantId = intent.getExtras().getString("plant_id");
         int notificationId = intent.getExtras().getInt("id");
-        Bitmap largeIcon = BitmapFactory.decodeResource(context.getResources(), R.drawable.logo_botanist);
+        Bitmap largeIcon = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_botanist_big);
         Intent resultIntent = new Intent(context, LoginActivity.class);
         PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.Builder mBuilder =
