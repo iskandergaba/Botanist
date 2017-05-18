85d28274c226792f3607ab782c8bac3a3360bb0f
Iskander Gaba <iskandergaba@gmail.com>
2017-05-18 13:20:44 +0100

    Added a GitHub Repo Link in About Activity

--- app/src/main/java/com/scientists/happy/botanist/ui/AboutActivity.java
+++ app/src/main/java/com/scientists/happy/botanist/ui/AboutActivity.java
@@ -1,23 +1,9 @@
 package com.scientists.happy.botanist.ui;
-/*
-Copyright 2016 Iskander Gaba
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.TextView;
 import com.scientists.happy.botanist.BuildConfig;
@@ -121,6 +107,8 @@ public class AboutActivity extends AppCompatActivity {
                 openWebPage(MARIMO_LINKEDIN_URL);
             }
         });
+        TextView fertilizationLink = (TextView)findViewById(R.id.github_repo_link);
+        fertilizationLink.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     /**
